02716f4cb8b8d6f6cd5b0ffb39f65e3f38d90c62
kenshi39847 <kenshi.t.blue@gmail.com>
2024-02-17 15:43:15 +0900

    コントローラの追記

--- src/main/java/com/example/demo/controller/RequestParamController.java
+++ src/main/java/com/example/demo/controller/RequestParamController.java
@@ -1,7 +1,13 @@
 package com.example.demo.controller;
 
+import java.time.LocalDate;
+
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class RequestParamController {
@@ -12,4 +18,15 @@ public class RequestParamController {
 		return "entry";
 	}
 	
+	//確認画面を表示する
+	@PostMapping("confirm")
+	public String confirmView(Model model, @RequestParam String name, @RequestParam Integer age, @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) @RequestParam LocalDate birth)
+	{
+		//Modelに格納する
+		model.addAttribute("name", name);
+		model.addAttribute("age", age);
+		model.addAttribute("birth", birth);
+		//戻り値はビュー名を返す
+		return "confirm";
+	}
 }
